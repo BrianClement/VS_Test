1887e339447ceda084f92d3a8f667d8d6b42c893
Brian Clement <Brian.Clement@scionresearch.com>
2017-04-20 12:46:09 +1200

    test the commit count

--- CppApp/CppApp.cpp
+++ CppApp/CppApp.cpp
@@ -1,12 +1,21 @@
 
 #include <iostream>
+#include "TestHeader2.h"
+#include <string>
 
 // CppApp.cpp : Defines the entry point for the console application.
 //
 
+using namespace std;
+
 int main()
 {
-	std::cout << "still more testing ..." << std::endl;
+	cout << "still more testing ..." << endl;
+
+	cout << TestValue << endl;
+	cout << "TestValue" << endl;
+	//cout << to_string( TestValue ) << endl;
+
 	return 0;
 }
 
